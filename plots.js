a9606cf25cbcc9f465000a206a3f685b087a3c75
guipotje <gui_potje@hotmail.com>
2021-07-08 03:17:49 -0300

    implement histogram of distances

--- plots.js
+++ plots.js
@@ -1,4 +1,7 @@
-
+// create a tooltip
+var Tooltip = d3.select("#div_label")
+  .append("div")
+  .attr("class", "tooltip")
 
 function colorbar(svg)
 {
@@ -111,11 +114,6 @@ function scatter(points, svg = null)
 
     colorbar(svg, myColor); //draw colorbar
 
-    // create a tooltip
-    var Tooltip = d3.select("#div_label")
-      .append("div")
-      .attr("class", "tooltip")
-
       // Three function that change the tooltip when user hover / move / leave a cell
       var mouseover = function(d) {
         Tooltip
@@ -199,6 +197,7 @@ function draw()
 {
     resize_canvas();
     draw_keypoints();
+    plot_histogram(data.dist_mat.mins);
 }
 
 
@@ -225,3 +224,128 @@ function draw_click(e){
     ctx.fill();
 }
 
+function plot_histogram(dists){
+
+  d3.select("#graph_svg").remove(); //clean old svg
+    // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 30, bottom: 30, left: 40},
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  var svg = d3.select("#graph")
+  .append("svg")
+  .attr("id","graph_svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+    .domain([data.dist_mat.min - 10, data.dist_mat.max + 10])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .range([0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Y axis: initialization
+  var y = d3.scaleLinear()
+    .range([height, 0]);
+  var yAxis = svg.append("g")
+
+
+
+  // A function that builds the graph for a specific value of bin
+  function update(nBin) {
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1);
+
+      d3.select(this)
+      .style("fill", "dimgray")
+      .style("stroke", "black")
+      .style("stroke-width", "2px")
+  }
+  var mousemove = function(d) {
+    var x = d3.event.pageX //- document.getElementById("graph").getBoundingClientRect().x + 10
+    var y = d3.event.pageY //-  document.getElementById("graph").getBoundingClientRect().y + 10
+    Tooltip
+      .html("" + d.length)
+      .style("left", (x-10) + "px")
+      .style("top", (y-40) + "px")
+      .style("z-index","4");
+
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+      .style("top","2px")
+      .style("left", "2px")
+      .style("z-index","-1");
+      
+      d3.select(this)
+      .style("fill", "rgb(90, 0, 237)")
+      .style("stroke", "none")
+      .style("stroke-width", "none")
+
+  }
+
+
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return d; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(nBin)); // then the numbers of bins
+
+  // And apply this function to data to get the bins
+  var bins = histogram(dists);
+
+  // Y axis: update now that we know the domain
+  y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  yAxis
+      .transition()
+      .duration(1000)
+      .call(d3.axisLeft(y));
+
+  // Join the rect with the bins data
+  var u = svg.selectAll("rect")
+      .data(bins)
+
+  // Manage the existing bars and eventually the new ones:
+  u
+      .enter()
+      .append("rect") // Add a new rect for each new elements
+      .merge(u) // get the already existing elements as well
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+      .transition() // and apply changes to all of them
+      .duration(1000)
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "rgb(90, 0, 237)")
+
+
+  // If less bar in the new histogram, I delete the ones not in use anymore
+  u
+      .exit()
+      .remove()
+
+  }
+
+  update(document.getElementById("nBin").value)
+
+
+  // Listen to the button -> update if user change it
+  d3.select("#nBin").on("input", function() {
+  update(+this.value);
+  });
+
+
+}
